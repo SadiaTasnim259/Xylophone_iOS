b13df93bf2e931235b8ebb608860515c88e222fb
Sadia Tasnim <tasnimsadia259@gmail.com>
2023-06-24 23:04:19 +0600

    AVFoundation is used for music

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -6,14 +6,27 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+ 
+    var player:AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        playSound(soundName:sender.currentTitle!)
+    }
+    
+    func playSound(soundName:String) {
+            
+            let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player!.play()
+            
+        }
 }
 
